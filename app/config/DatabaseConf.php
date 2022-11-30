e418010ae96a6ab7530369f72984dc82014c7d81
Sandul Renuja <sandulrenuja@gmail.com>
2022-11-30 22:04:39 +0530

    Define constants for project
    Signed-off-by: Sandul Renuja <sandulrenuja@gmail.com>

---
+++ app/config/DatabaseConf.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Defines constant values used for connecting to DB
+ */
+
+const DB_TYPE = 'mysql';
+const DB_HOST = 'localhost';
+const DB_PORT = '3306';
+const DB_USER = 'root';
+const DB_NAME = 'krushi-arunalu-db';
+const DB_PASSWORD = 'root';
