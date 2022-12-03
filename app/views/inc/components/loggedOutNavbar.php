99236c8862d51c35d2878f0e75070c3ae56a9eae
Sandul Renuja <sandulrenuja@gmail.com>
2022-12-03 11:59:12 +0530

    Add styles to login page
    Signed-off-by: Sandul Renuja <sandulrenuja@gmail.com>

---
+++ app/views/inc/components/loggedOutNavbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+    <div class="container">
+        <div class="">
+            <img class="brand-logo" src="<?php echo URL_ROOT ?>/public/img/navbar-logo.webp" alt="logo" height="72px">
+        </div>
+        <div class="">
+            <a class="nav-link" href="/login">Home</a>
+        </div>
+    </div>
+</nav>
