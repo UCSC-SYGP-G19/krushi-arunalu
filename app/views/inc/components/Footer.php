83567bd64b17d46d31df3a3142aa37243edc4211
Sandul Renuja <sandulrenuja@gmail.com>
2022-12-04 17:15:58 +0530

    Modify footer component

--- app/views/inc/components/Footer.php
+++ app/views/inc/components/Footer.php
@@ -1,14 +1,10 @@
-<footer class="container-fluid footer">
+<?php
+
+echo '<footer class="container-fluid footer">
     <div class="row">
         <div class="col-12 text-center justify-content-center p-2">
             <h5 class="fw-normal">Crafted with ♥️ by CS Group 19</h5>
             <p class="fs-2 text-grey-dark">Copyright ©️ 2022</p>
         </div>
     </div>
-</footer>
-
-<?php
-echo "</html>";
-?>
-
-
+</footer>';
