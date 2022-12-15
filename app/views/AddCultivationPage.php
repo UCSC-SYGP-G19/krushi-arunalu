1326bdc2c6375b0992773b6d45fa91c0f153b826
Sandul Renuja <sandulrenuja@gmail.com>
2022-12-15 23:32:59 +0530

    Resolve minor issues

--- app/views/AddCultivationPage.php
+++ app/views/AddCultivationPage.php
@@ -4,10 +4,7 @@ include APP_ROOT . "/views/inc/components/Header.php";
 ?>
 <?php
 
-if (isset($this->user)) {
-    echo "Logged in as: " . $this->user->getName() . " (" . $this->user->getRole() . ")<br>";
-    echo "<a href='./logout'>Logout</a>";
-} else {
+if (!isset($this->user)) {
     echo "You are not logged in, please <a href='./login'>login</a>";
 }
 
