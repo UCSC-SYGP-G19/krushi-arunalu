b9ed8fec72090c7dd5d03208c6f83fc120b33e82
SuraniDS <suranisooriyaarachchi99429@gmail.com>
2022-12-16 01:53:00 +0530

    Implement Product for Manufacturers

---
+++ app/models/Product.php
@@ -0,0 +1,235 @@
+<?php
+
+/**
+ * @file
+ * Model to represent the Product table in the database
+ * Contains both attributes and methods related to the Product entity
+ */
+
+namespace app\models;
+
+use app\core\Model;
+
+class Product extends Model
+{
+    private ?int $id;
+    private ?string $name;
+    private ?string $imageUrl;
+    private ?string $description;
+    private ?float $weight;
+    private ?string $unit;
+    private ?float $unitSellingPrice;
+    private ?float $stockQuantity;
+    private ?int $manufacturerId;
+    private ?int $categoryId;
+
+    public function __construct(
+        $id = null,
+        $name = null,
+        $imageUrl = null,
+        $description = null,
+        $weight = null,
+        $unit = null,
+        $unitPrice = null,
+        $stockQuantity = null,
+        $manufacturerId = null,
+        $categoryId = null,
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->imageUrl = $imageUrl;
+        $this->description = $description;
+        $this->weight = $weight;
+        $this->unit = $unit;
+        $this->unitSellingPrice = $unitPrice;
+        $this->stockQuantity = $stockQuantity;
+        $this->manufacturerId = $manufacturerId;
+        $this->categoryId = $categoryId;
+    }
+
+    public function addProductToDB(): bool
+    {
+        $result = $this->runQuery(
+            'INSERT into product (name, image_url, description, 
+                     weight, unit, unit_selling_price, stock_quantity, manufacturer_id, category_id) VALUES 
+                         (?,?,?,?,?,?,?,?,?)',
+            [$this->name, $this->imageUrl, $this->description, $this->weight, $this->unit,
+                $this->unitSellingPrice, $this->stockQuantity, $this->manufacturerId, $this->categoryId]
+        );
+        return $result == true;
+    }
+
+    public function getProductsByManufacturerId($manufacturerId): array
+    {
+        $result = $this->runQuery("SELECT 
+            product.image_url as 'image_url', 
+            product_category.name as 'category_name',
+            product.name as 'product_name',
+            product.stock_quantity as stock_qty,
+            product.unit_selling_price as 'unit_price'
+            FROM product
+            INNER JOIN product_category ON product.category_id = product_category.id WHERE product.manufacturer_id = ?
+            ", [$manufacturerId])->fetchAll();
+        return $result;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     */
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string|null $description
+     */
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getWeight(): ?float
+    {
+        return $this->weight;
+    }
+
+    /**
+     * @param float|null $weight
+     */
+    public function setWeight(?float $weight): void
+    {
+        $this->weight = $weight;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUnit(): ?string
+    {
+        return $this->unit;
+    }
+
+    /**
+     * @param string|null $unit
+     */
+    public function setUnit(?string $unit): void
+    {
+        $this->unit = $unit;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getImageUrl(): ?string
+    {
+        return $this->imageUrl;
+    }
+
+    /**
+     * @param string|null $imageUrl
+     */
+    public function setImageUrl(?string $imageUrl): void
+    {
+        $this->imageUrl = $imageUrl;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getUnitSellingPrice(): ?float
+    {
+        return $this->unitSellingPrice;
+    }
+
+    /**
+     * @param float|null $unitSellingPrice
+     */
+    public function setUnitSellingPrice(?float $unitSellingPrice): void
+    {
+        $this->unitSellingPrice = $unitSellingPrice;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getStockQuantity(): ?float
+    {
+        return $this->stockQuantity;
+    }
+
+    /**
+     * @param float|null $stockQuantity
+     */
+    public function setStockQuantity(?float $stockQuantity): void
+    {
+        $this->stockQuantity = $stockQuantity;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getManufacturerId(): ?int
+    {
+        return $this->manufacturerId;
+    }
+
+    /**
+     * @param int|null $manufacturerId
+     */
+    public function setManufacturerId(?int $manufacturerId): void
+    {
+        $this->manufacturerId = $manufacturerId;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getCategoryId(): ?int
+    {
+        return $this->categoryId;
+    }
+
+    /**
+     * @param int|null $categoryId
+     */
+    public function setCategoryId(?int $categoryId): void
+    {
+        $this->categoryId = $categoryId;
+    }
+}
