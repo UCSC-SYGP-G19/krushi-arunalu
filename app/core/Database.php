3364e26f16a328dd0f17a9675a4000c756ba23bd
Sandul Renuja <sandulrenuja@gmail.com>
2023-01-11 00:20:33 +0530

    Change PDO fetch mode

--- app/core/Database.php
+++ app/core/Database.php
@@ -33,7 +33,7 @@ class Database
         array $options = []
     ): ?PDO {
         $default_options = [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
             PDO::ATTR_EMULATE_PREPARES => false,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         ];
