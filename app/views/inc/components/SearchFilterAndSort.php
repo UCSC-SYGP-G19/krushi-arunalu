fa2bccef2cf03c7fbc920c6d422b621ed92a116c
Sandul Renuja <sandulrenuja@gmail.com>
2023-01-26 01:10:05 +0530

    Add icons to table search, filter and sort options

--- app/views/inc/components/SearchFilterAndSort.php
+++ app/views/inc/components/SearchFilterAndSort.php
@@ -1,19 +1,19 @@
 <div class="row table-controls gap-1">
     <div class="col-8">
         <label>
-            <input type="text" placeholder="Search"/>
+            <input type="text" placeholder="Search" class="with-search-icon"/>
         </label>
     </div>
     <div class="col-2">
         <label>
-            <select name="table_filter" id="table_filter">
+            <select name="table_filter" id="table_filter" class="with-filter-icon">
                 <option value="">Filter</option>
             </select>
         </label>
     </div>
     <div class="col-2">
         <label>
-            <select name="table_sort" id="table_sort">
+            <select name="table_sort" id="table_sort" class="with-sort-icon">
                 <option value="">Sort</option>
             </select>
         </label>
