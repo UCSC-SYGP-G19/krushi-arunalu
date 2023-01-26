316a3374533be56cfd922cc91037b98b70ba69ce
Sandul Renuja <sandulrenuja@gmail.com>
2023-01-27 02:20:56 +0530

    Add Error 403 page and rename icons

---
+++ app/views/inc/components/LoggedOutNavbarWithLoginLink.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+    <div class="pt-1">
+        <img class="brand-logo-large" src="<?php echo URL_ROOT ?>/public/img/navbar-logo-large.webp" alt="logo"
+             height="72px">
+    </div>
+    <div class="pt-1">
+        <a class="nav-link mx-3" href="<?php echo URL_ROOT ?>">Home</a>
+        <a class="nav-link mx-3" href="<?php echo URL_ROOT ?>/login">Login/Register</a>
+    </div>
+</nav>
