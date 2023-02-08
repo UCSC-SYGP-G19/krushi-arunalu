8e67f572cd1664d5ad6acf4c0d79b123bcf5a712
SuraniDS <suranisooriyaarachchi99429@gmail.com>
2023-02-08 16:07:47 +0530

    Create the view producers in manufacturer

---
+++ app/controllers/ProducersController.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Controller which handles product categories of Manufacturers
+ */
+
+namespace app\controllers;
+
+use app\core\Controller;
+use app\helpers\Util;
+
+class ProducersController extends Controller
+{
+    public function index(): void
+    {
+        $this->loadView('Manufacturer/ProducersPage', 'Producers', 'producers');
+        $this->loadModel("Producer");
+        $this->view->data = $this->model->getAllFromDB();
+        $this->view->render();
+    }
+
+    public function connectionRequests(): void
+    {
+        $this->loadView('Manufacturer/ConnectionRequestsPage', 'Connection Requests', 'producers');
+
+        $this->view->render();
+    }
+}
--- app/models/Producer.php
+++ app/models/Producer.php
@@ -38,6 +38,16 @@ class Producer extends RegisteredUser
         return false;
     }
 
+    public function getAllFromDB(): array
+    {
+        return $this->runQuery("SELECT 
+            producer.id as 'producer_id',     
+            registered_user.name as 'producer_name'
+            FROM producer
+            INNER JOIN registered_user ON producer.id = registered_user.id
+            ", [])->fetchAll();
+    }
+
     /**
      * @return string|null
      */
