2dcc0fb9a9010286b88df5e4c38c5dd2808e99ed
VinuriGamage <89873897+VinuriGamage@users.noreply.github.com>
2023-02-10 09:25:00 +0530

    Resolve issues in shopping cart

--- app/views/Customer/ShoppingCartPage.php
+++ app/views/Customer/ShoppingCartPage.php
@@ -43,7 +43,7 @@ include APP_ROOT . "/views/inc/components/Header.php";
 
                                 <table>
                                     <thead>
-                                    <tr class="row">
+                                    <tr class="row py-3">
                                         <th class="col-1"></th>
                                         <th class="col-4">Product Name</th>
                                         <th class="col-2">Quantity</th>
@@ -84,34 +84,43 @@ include APP_ROOT . "/views/inc/components/Header.php";
                                     ?>
                                     </tbody>
                                     <tfoot>
-                                    <tr class="row justify-content-end pagination">
-                                        <td class="col-3 text-right"><span>Rows per page:</span><label>
-                                                <select name="table_filter" id="table_filter">
-                                                    <option value=""><?php echo count($this->data); ?></option>
-                                                </select>
-                                            </label></td>
-                                        <td class="col-2">1-<?php echo count($this->data); ?> of
-                                            <?php echo count($this->data); ?>
-                                            <span class="arrow-icons">
-                                                <span class="left-arrow">
-                                                    <svg width="9" height="15" viewBox="0 0 9 15" fill="none"
-                                                         xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M7.10107 13.4121L1.10107 7.41211L7.10107 1.41211"
-                                                          stroke="#B1B1B1" stroke-width="2" stroke-linecap="round"
-                                                          stroke-linejoin="round"/>
-                                                </svg>
-                                                </span>
+                                    <tr class="row table-summary-row py-3">
+                                        <td class="col-8 justify-content-start"><strong>Total</strong></td>
+                                        <td class="col-2"></td>
+                                        <td class="col-2"></td>
+                                    </tr>
+<!---->
+<!--                                    <tr class="row justify-content-end pagination">-->
+<!--                                        <td class="col-3 text-right"><span>Rows per page:</span><label>-->
+<!--                                                <select name="table_filter" id="table_filter">-->
+<!--                                                    <option value="">--><?php //echo count($this->data); ?><!--</option>-->
+<!--                                                </select>-->
+<!--                                            </label></td>-->
+<!--                                        <td class="col-2">1---><?php //echo count($this->data); ?><!-- of-->
+<!--                                            --><?php //echo count($this->data); ?>
+<!--                                            <span class="arrow-icons">-->
+<!--                                                <span class="left-arrow">-->
+<!--                                                    <svg width="9" height="15" viewBox="0 0 9 15" fill="none"-->
+<!--                                                         xmlns="http://www.w3.org/2000/svg">-->
+<!--                                                    <path d="M7.10107 13.4121L1.10107 7.41211L7.10107 1.41211"-->
+<!--                                                          stroke="#B1B1B1" stroke-width="2" stroke-linecap="round"-->
+<!--                                                          stroke-linejoin="round"/>-->
+<!--                                                </svg>-->
+<!--                                                </span>-->
+<!---->
+<!--                                                <span class="right-arrow">-->
+<!--                                                    <svg width="9" height="15" viewBox="0 0 9 15" fill="none"-->
+<!--                                                         xmlns="http://www.w3.org/2000/svg">-->
+<!--                                                    <path d="M1.854 13.3516L7.854 7.35156L1.854 1.35156"-->
+<!--                                                          stroke="#B1B1B1" stroke-width="2" stroke-linecap="round"-->
+<!--                                                          stroke-linejoin="round"/>-->
+<!--                                                </svg>-->
+<!--                                                </span>-->
+<!--                                            </span>-->
+<!--                                        </td>-->
+<!--                                    -->
+<!--                                    -->
 
-                                                <span class="right-arrow">
-                                                    <svg width="9" height="15" viewBox="0 0 9 15" fill="none"
-                                                         xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M1.854 13.3516L7.854 7.35156L1.854 1.35156"
-                                                          stroke="#B1B1B1" stroke-width="2" stroke-linecap="round"
-                                                          stroke-linejoin="round"/>
-                                                </svg>
-                                                </span>
-                                            </span>
-                                        </td>
                                     </tfoot>
                                 </table>
                             </div>
