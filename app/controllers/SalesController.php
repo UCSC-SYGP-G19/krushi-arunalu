76151884bd7ff8cfd78c18e26ce55cd1292ecc24
SRenuuu <sandulrenuja@gmail.com>
2023-03-10 19:04:21 +0530

    Update Sales page for producers
    Signed-off-by: SRenuuu <sandulrenuja@gmail.com>

--- app/controllers/SalesController.php
+++ app/controllers/SalesController.php
@@ -10,28 +10,48 @@ namespace app\controllers;
 use app\core\Controller;
 use app\helpers\Session;
 use app\helpers\Util;
+use app\models\ManufacturerOrder;
 
 class SalesController extends Controller
 {
+    public string $base = URL_ROOT . "/sales";
     public function index(): void
     {
         $this->loadView("Producer/SalesPage", "Sales", "sales");
-        $this->loadModel("ManufacturerOrder");
-        $this->view->data = $this->model->getAllOrdersByProducerId(Session::getSession()->id);
+        $this->view->data = ManufacturerOrder::getAllOrdersByProducerId(Session::getSession()->id);
         $this->view->render();
     }
 
     public function accept($orderId): void
     {
         $this->loadModel("ManufacturerOrder");
-        $this->model->acceptOrder($orderId);
-        Util::redirect("sales");
+        $this->model->fillData([
+            "orderId" => $orderId,
+            "status" => "Accepted"
+        ]);
+        $this->model->updateOrderStatusInDB();
+        Util::redirect($this->base);
     }
 
-    public function decline($orderId): void
+    public function reject($orderId): void
     {
         $this->loadModel("ManufacturerOrder");
-        $this->model->declineOrder($orderId);
-        Util::redirect("sales");
+        $this->model->fillData([
+            "orderId" => $orderId,
+            "status" => "Rejected"
+        ]);
+        $this->model->updateOrderStatusInDB();
+        Util::redirect($this->base);
+    }
+
+    public function ship($orderId): void
+    {
+        $this->loadModel("ManufacturerOrder");
+        $this->model->fillData([
+            "orderId" => $orderId,
+            "status" => "Shipped"
+        ]);
+        $this->model->updateOrderStatusInDB();
+        Util::redirect($this->base);
     }
 }
--- app/views/Producer/SalesPage.php
+++ app/views/Producer/SalesPage.php
@@ -1,7 +1,5 @@
 <?php
 
-use app\views\inc\components\Table;
-
 include APP_ROOT . "/views/inc/components/Header.php";
 
 ?>
@@ -29,66 +27,113 @@ include APP_ROOT . "/views/inc/components/Header.php";
                                 <br>
                                 <?php
                                 include APP_ROOT . "/views/inc/components/SearchFilterAndSort.php";
-                                $this->tableHeaders = [
-                                    "order_id" => [
-                                        "label" => "Order ID",
-                                        "sortable" => true,
-                                        "sortKey" => "order_id",
-                                        "class" => "col-1",
-                                    ],
-                                    "order_date" => [
-                                        "label" => "Date",
-                                        "sortable" => true,
-                                        "sortKey" => "date",
-                                        "class" => "col-1",
-                                    ],
-                                    "crop_name" => [
-                                        "label" => "Crop name",
-                                        "sortable" => true,
-                                        "sortKey" => "crop_name",
-                                        "class" => "col-2",
-                                    ],
-                                    "quantity" => [
-                                        "label" => "Qty",
-                                        "sortable" => true,
-                                        "sortKey" => "quantity",
-                                        "class" => "col-1",
-                                    ],
-                                    "unit_selling_price" => [
-                                        "label" => "Price per unit",
-                                        "sortable" => true,
-                                        "sortKey" => "unit_selling_price",
-                                        "class" => "col-1"
-                                    ],
-                                    "manufacturer_name" => [
-                                        "label" => "Manufacturer",
-                                        "sortable" => true,
-                                        "sortKey" => "Manufacturer",
-                                        "class" => "col-2"
-                                    ],
-                                    "order_status" => [
-                                        "label" => "Status",
-                                        "sortable" => true,
-                                        "sortKey" => "status",
-                                        "class" => "col-2"
-                                    ],
-                                    "actions" => [
-                                        "label" => "",
-                                        "sortable" => false,
-                                        "class" => "col-2"
-                                    ]
-                                ];
-                                $salesTable = new Table(
-                                    "sales",
-                                    $this->tableHeaders,
-                                    $this->data,
-                                    "order_id",
-                                    "No orders yet",
-                                    ["Accept", "Decline"],
-                                    ["accept", "decline"]
-                                );
-                                $salesTable->render();
                                 ?>
+                                <table>
+                                    <thead>
+                                    <tr class="row">
+                                        <th class="col-1">Order ID</th>
+                                        <th class="col-1">Date</th>
+                                        <th class="col-2">Status</th>
+                                        <th class="col-2">Manufacturer</th>
+                                        <th class="col-2">Crop name</th>
+                                        <th class="col-1">Quantity (KG)</th>
+                                        <th class="col-1">Order total (Rs.)</th>
+
+
+                                        <th class="col-2"></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php
+                                    function generateActions($orderId, $orderStatus): void
+                                    {
+                                        switch ($orderStatus) {
+                                            case "Pending":
+                                                echo '<a class="btn-xs btn-outlined-primary-dark m-1" 
+                                                    href = "' . URL_ROOT . '/sales/accept/' . $orderId . '">Accept
+                                                    </a>';
+                                                echo '<a class="btn-xs btn-outlined-error-dark m-1" 
+                                                    href = "' . URL_ROOT . '/sales/reject/' . $orderId . '">Reject
+                                                    </a>';
+                                                break;
+                                            case "Accepted":
+                                                echo '<a class="btn-xs btn-outlined-secondary m-1" 
+                                                    href = "' . URL_ROOT . '/sales/ship/' . $orderId . '">Ship items
+                                                    </a>';
+                                                break;
+                                            default:
+                                                break;
+                                        }
+                                    }
+
+                                    function renderStatus($orderStatus): void
+                                    {
+                                        echo match ($orderStatus) {
+                                            "Pending" => '<span class="badge badge-warning">' . $orderStatus .
+                                                '</span>',
+                                            "Accepted" => '<span class="badge badge-primary">' . $orderStatus .
+                                                '</span>',
+                                            "Rejected" => '<span class="badge badge-danger">' . $orderStatus .
+                                                '</span>',
+                                            "Delivered" => '<span class="badge badge-success">' . $orderStatus .
+                                                '</span>',
+                                            "Shipped" => '<span class="badge badge-secondary">' . $orderStatus .
+                                                '</span>',
+                                            default => '<span>' . $orderStatus . '</span>',
+                                        };
+                                    }
+
+                                    foreach ($this->data as $order) {
+                                        ?>
+                                        <tr class="row">
+                                            <td class="col-1"><?php echo $order->order_id; ?></td>
+                                            <td class="col-1"><?php echo $order->order_date; ?></td>
+                                            <td class="col-2"><?php renderStatus($order->order_status); ?></td>
+                                            <td class="col-2"><?php echo $order->manufacturer_name; ?></td>
+                                            <td class="col-2"><?php echo $order->crop_name; ?></td>
+                                            <td class="col-1"><?php echo $order->order_quantity; ?></td>
+                                            <td class="col-1"><?php echo
+                                                number_format($order->order_total, 2); ?></td>
+                                            <td class="col-2 px-1 py-1">
+                                                <div class="row justify-content-center align-items-center p-1">
+                                                    <?php generateActions($order->order_id, $order->order_status) ?>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                    }
+                                    ?>
+                                    </tbody>
+                                    <tfoot>
+                                    <tr class="row justify-content-end pagination">
+                                        <td class="col-3 text-right"><span>Rows per page:</span><label>
+                                                <select name="table_filter" id="table_filter">
+                                                    <option value="">10</option>
+                                                </select>
+                                            </label></td>
+                                        <td class="col-2">1-2 of 25
+                                            <span class="arrow-icons">
+                                                <span class="left-arrow">
+                                                    <svg width="9" height="15" viewBox="0 0 9 15" fill="none"
+                                                         xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M7.10107 13.4121L1.10107 7.41211L7.10107 1.41211"
+                                                          stroke="#B1B1B1" stroke-width="2" stroke-linecap="round"
+                                                          stroke-linejoin="round"/>
+                                                </svg>
+                                                </span>
+
+                                                <span class="right-arrow">
+                                                    <svg width="9" height="15" viewBox="0 0 9 15" fill="none"
+                                                         xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M1.854 13.3516L7.854 7.35156L1.854 1.35156"
+                                                          stroke="#B1B1B1" stroke-width="2" stroke-linecap="round"
+                                                          stroke-linejoin="round"/>
+                                                </svg>
+                                                </span>
+                                            </span>
+                                        </td>
+                                    </tfoot>
+                                </table>
                             </div>
                         </div>
                     </div>
