b032052e079d3287fdc7d15071d2f07c42abcee1
SRenuuu <sandulrenuja@gmail.com>
2023-05-07 10:36:58 +0530

    Do minor UI modifications
    Signed-off-by: SRenuuu <sandulrenuja@gmail.com>

--- app/views/inc/components/LoggedInNavbarWithoutSidebar.php
+++ app/views/inc/components/LoggedInNavbarWithoutSidebar.php
@@ -10,13 +10,24 @@
         <div class="row">
             <div class="col-5 m-auto text-center">
                 <div class="justify-content-end align-items-center row">
-                    <div id="bell-icon" class="navbar-icons mx-2">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M18 8C18 6.4087 17.3679 4.88258 16.2426 3.75736C15.1174 2.63214 13.5913 2 12 2C10.4087 2 8.88258 2.63214 7.75736 3.75736C6.63214 4.88258 6 6.4087 6 8C6 15 3 17 3 17H21C21 17 18 15 18 8Z"
-                                  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M13.7295 21C13.5537 21.3031 13.3014 21.5547 12.9978 21.7295C12.6941 21.9044 12.3499 21.9965 11.9995 21.9965C11.6492 21.9965 11.3049 21.9044 11.0013 21.7295C10.6977 21.5547 10.4453 21.3031 10.2695 21"
-                                  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
+                    <div class="position-relative">
+                        <button id="btn-toggle-notification-panel" class="navbar-icons mx-2">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M18 8C18 6.4087 17.3679 4.88258 16.2426 3.75736C15.1174 2.63214 13.5913 2 12 2C10.4087 2 8.88258 2.63214 7.75736 3.75736C6.63214 4.88258 6 6.4087 6 8C6 15 3 17 3 17H21C21 17 18 15 18 8Z"
+                                      stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M13.7295 21C13.5537 21.3031 13.3014 21.5547 12.9978 21.7295C12.6941 21.9044 12.3499 21.9965 11.9995 21.9965C11.6492 21.9965 11.3049 21.9044 11.0013 21.7295C10.6977 21.5547 10.4453 21.3031 10.2695 21"
+                                      stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <div id="notifications-panel">
+                            <div class="pt-2 pb-4 px-2 justify-content-center text-center">
+                                <img src="<?php echo URL_ROOT ?>/public/img/other/no-notifications.gif"
+                                     alt="Sleeping bell"
+                                     width="240px">
+                                <h5 class="fw-normal text-secondary text-center mb-1 py-1">No notifications</h5>
+                            </div>
+                        </div>
                     </div>
 
                     <a href="./chat" class="mx-2">
@@ -62,14 +73,6 @@
             </div>
         </div>
 
-    </div>
-
-    <div id="notifications-panel">
-        <div class="pt-2 pb-4 px-2 justify-content-center text-center">
-            <img src="<?php echo URL_ROOT ?>/public/img/other/no-notifications.gif" alt="Sleeping bell"
-                 width="240px">
-            <h5 class="fw-normal text-secondary text-center mb-1 py-1">No notifications</h5>
-        </div>
     </div>
 
     <div id="navbar-options-panel" class="py-1">
