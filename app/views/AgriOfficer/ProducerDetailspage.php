fbd23c45d8eeba0ba0a70dad9f55e5cef50eadc6
SRenuuu <sandulrenuja@gmail.com>
2023-05-09 20:16:16 +0530

    Refactor code and fix footer issue in Announcements page
    Signed-off-by: SRenuuu <sandulrenuja@gmail.com>

--- app/views/AgriOfficer/ProducerDetailspage.php
+++ app/views/AgriOfficer/ProducerDetailspage.php
@@ -94,6 +94,7 @@ include APP_ROOT . "/views/inc/components/Header.php";
                     </div>
                 </div>
             </div>
+
             <?php
             include APP_ROOT . "/views/inc/components/Footer.php";
             ?>
