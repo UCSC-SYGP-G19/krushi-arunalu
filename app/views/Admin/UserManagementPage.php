48a2fe00330cd4537106ea42543ad8b7c3d53e32
Rumindu De Silva <rumindukavishka14@gmail.com>
2023-05-14 17:19:59 +0530

    Create admin user-management-page

--- app/views/Admin/UserManagementPage.php
+++ app/views/Admin/UserManagementPage.php
@@ -21,7 +21,57 @@ include APP_ROOT . "/views/inc/components/Header.php";
             include APP_ROOT . "/views/inc/components/LoggedInNavbar.php"
             ?>
 
+            <div class="content-wrapper">
+                <div class="content p-4 mt-1">
+                    <div class="container-fluid px-2">
+                        <div class="row px-1 pt-1 justify-content-space-between">
+                            <div class="col-6">
+                                <h1 class="title">Agri Officers</h1>
+                            </div>
+                            <div class="col">
+                                <a href="<?php echo URL_ROOT ?>/UserManagement/addAgriOfficer"
+                                   class="btn-md btn-primary-light text-center text-white">
+                                    Add Agri Officer</a>
+                            </div>
+                        </div>
+                        <div class="row px-1 pt-2">
+                            <div class="col-12 text-justify">
+                                <br>
+                                <table>
+                                    <thead>
+                                    <tr class="row">
+                                        <th class="col-2">NIC</th>
+                                        <th class="col-3">Name</th>
+                                        <th class="col-2">Contact no</th>
+                                        <th class="col-3">Email</th>
+                                        <th class="col-2"></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php foreach ($this->data as $row) { ?>
+                                        <tr class="row">
+                                            <td class="col-2"><?php echo $row->nic; ?></td>
+                                            <td class="col-3"><?php echo $row->name; ?></td>
+                                            <td class="col-2"><?php echo $row->contact_no; ?></td>
+                                            <td class="col-3"><?php echo $row->email; ?></td>
+                                            <td class="col-2 pr-3">
+                                                <div class="row align-items-center justify-content-center gap-1">
+                                                    <a href=''
+                                                       class="btn-xs btn-outlined-primary-dark text-center">
+                                                        View Details
+                                                    </a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    <?php } ?>
 
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <?php
             include APP_ROOT . "/views/inc/components/Footer.php";
             ?>
