1fa85cc827017b0a8211a7460686400acc487bc3
VinuriGamage <89873897+VinuriGamage@users.noreply.github.com>
2023-05-14 05:43:15 +0530

    update login process

--- app/views/inc/components/LoggedOutNavbarWithLoginLink.php
+++ app/views/inc/components/LoggedOutNavbarWithLoginLink.php
@@ -6,9 +6,11 @@
                 <span class="col-2 px-1 py-2 fs-4 fw-bold"><a href="#" class="tab-btn">About Us</a></span>
                 <span class="col-2 px-1 py-2 fs-4 fw-bold"><a href="#" class="tab-btn">Contact Us</a></span>
                 <div class="col-4 d-flex px-2 justify-content-center">
-                    <button class="col-6 px-2 mx-2 btn-primary-light text-white align-items-center"><a href="./register/" class="tab-btn">
+                    <button class="col-6 px-2 mx-2 btn-primary-light text-white align-items-center">
+                        <a href="./register/registerCustomer/" class="tab-btn">
                             Register</button>
-                    <button class="col-6 px-2 btn-outlined-primary-dark text-center align-items-center"><a href="./login/" class="tab-btn">Login</button>
+                    <button class="col-6 px-2 btn-outlined-primary-dark text-center align-items-center">
+                        <a href="<?php echo URL_ROOT ?>/Login" class="tab-btn">Login</button>
                 </div>
             </div>
         </div>
